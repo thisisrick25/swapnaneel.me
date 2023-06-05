0095421d7d42f1c28ffffece3cbdf45145f149b5
thisisrick25 <swapnaneel06@gmail.com>
2023-06-05 21:17:26 +0530

    feat: add header

---
+++ app/components/header.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Navbar from '@/navbar'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <Link>
+            </Link>
+        </div>
+        <div>
+            <Navbar />
+        </div>
+    </header>
+  )
+}
