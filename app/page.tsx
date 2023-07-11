080c547b4bd6e46c5b930df229b6759faf32946b
thisisrick25 <swapnaneel06@gmail.com>
2023-07-11 12:21:19 +0530

    add caption

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=' overflow-x-hidden pb-32 h-screen flex items-center justify-center'>
-      <div>
+    <div className='h-screen flex flex-col items-center justify-center'>
+      <p className='text-2xl font-bold'>
         hello, Im Swapnaneel
-      </div>
-      {/* <div>
-        software engineer, technologist, and entrepreneur
-      </div> */}
+      </p>
+      <p className='text-xl'>
+        software engineer and technologist
+      </p>
     </div>
   )
 }
