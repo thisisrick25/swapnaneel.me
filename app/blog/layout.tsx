6d546d4595078711d3b32154d01a6e6ce024eb89
thisisrick25 <swapnaneel06@gmail.com>
2023-07-25 01:44:13 +0530

    edit layout for blog page

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,6 +4,8 @@ export default function BlogLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className="grid grid-cols-3">{children}</section>
+    <div className='grid grid-col-1 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto px-4 sm:px-6 xl:px-0 gap-10 sm:gap-14'>
+      {children}
+    </div>
   )
 }
