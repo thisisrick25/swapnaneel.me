87ef491f768db22e77f882df49b0b7684caba77f
thisisrick25 <swapnaneel06@gmail.com>
2023-07-25 01:47:22 +0530

    change border color when dark

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import { HomeLogo } from './homeLogo'
 
 export default function Header() {
   return (
-    <header className='w-full sticky z-20 top-0 grid grid-cols-6 justify-between py-3 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 border-b border-gray-200 dark:border-gray-800 backdrop-filter backdrop-saturate-150 backdrop-blur-lg'>
+    <header className='w-full sticky z-20 top-0 grid grid-cols-6 justify-between py-3 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 border-b border-gray-200 dark:border-neutral-700 backdrop-filter backdrop-saturate-150 backdrop-blur-lg'>
       <Link
         href='/'
         className='col-start-2 justify-self-start'
