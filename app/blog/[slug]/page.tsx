92ac7bc961f82a359511f40942d0d004e8c17094
thisisrick25 <swapnaneel06@gmail.com>
2023-07-25 22:58:02 +0530

    feat: add blog slug page

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from 'next'
+import { allBlogs } from 'contentlayer/generated'
+import { getBlog, getAllBlogs, getSeries } from "lib/content";
+
+export async function generateStaticParams() {
+  return allBlogs
+    .filter((p) => p.status != "draft")
+    .map((p) => {
+      slug: p.slug;
+    });
+}
+
+// @ts-ignore
+export async function generateMetadata({ params }) {
+  const blog = await getBlog(params.slug);
+
+  return {
+    title: `${blog.title} | Swapnaneel`,
+    description: blog.description,
+    authors: {
+      name: "Swapnaneel Patra",
+      url: "https://swapnaneel.me",
+    },
+    keywords: blog.tags?.map((tag) => tag.title),
+    creator: "Swapnaneel Patra",
+    twitter: {
+      card: "summary_large_image",
+      creator: "@thisisrick25",
+    },
+  };
+}
+
+// @ts-ignore
+export default function Page({ params }) {
+  const blog = allBlogs.find((post) => post.slug === params.slug);
+
+  return (
+    <>
+      <div>{blog?.title}</div>
+      <div>{blog?.description}</div>
+    </>
+  )
+}
