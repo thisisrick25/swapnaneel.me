903cafd16a7ba3842085b277efc16c6cdc92da26
thisisrick25 <swapnaneel06@gmail.com>
2023-08-04 23:07:20 +0530

    refactor: common layout for page and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     // suppressHydrationWarning
     <html lang="en" className={roboto.className} suppressHydrationWarning>
-      <body className='bg-white text-black dark:bg-black dark:text-white text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl selection:bg-yellow-500 selection:text-white dark:selection:text-black' suppressHydrationWarning>
+      <body className='bg-white text-black dark:bg-black dark:text-white text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl selection:bg-yellow-500 selection:text-white dark:selection:text-black max-w-2xl px-4 mx-auto sm:px-6 xl:max-w-3xl xl:px-0 sm:gap-14' suppressHydrationWarning>
         <ThemeProvider>
           <Header />
           {children}
