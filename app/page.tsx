c57568f035b505273bd006942eb47ff9e364c2bc
thisisrick25 <swapnaneel06@gmail.com>
2023-08-04 23:07:44 +0530

    refactor: simplify home

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='grid grid-cols-1 max-w-2xl px-4 mx-auto sm:px-6 xl:max-w-3xl xl:px-0 gap-10 sm:gap-14'>
-      <div className=''>
-        <div className='text-2xl font-bold'>
-          hello, Im Swapnaneel
+    <div className='grid grid-cols-1'>
+      <div className='p-12'>
+        <div className='text-xl font-bold'>
+          hello, im Swapnaneel
         </div>
-        <div className='text-xl'>
-          software engineer and technologist
-        </div>
-        <div>
-          <p>Lorem ipsum dolor sit amet. Et veritatis inventore rem distinctio perspiciatis eos quis similique ea voluptatem necessitatibus et dolorum dolor aut tempore galisum est internos magnam. Sit facilis impedit ut excepturi quasi ut itaque magnam. </p><p>In porro commodi et consequuntur quod sed Quis perferendis. Id animi obcaecati sit veritatis eaque et debitis dolor et molestias voluptatem cum aliquid tempore aut reiciendis adipisci et architecto galisum. Aut error consectetur aut numquam internos ut consequatur sunt ut necessitatibus ullam 33 dolorum magni et vitae quisquam. Ex facere necessitatibus et ducimus voluptatem quo facere quaerat eum consequatur sapiente ex sint explicabo eum magnam saepe quo velit dignissimos. </p><p>At distinctio reprehenderit et minus eveniet sed rerum sunt a voluptatem magnam. Et aspernatur voluptas sit maiores facilis et distinctio reprehenderit eum magni velit cum amet quibusdam 33 fugiat consequatur. </p><p>In accusantium eius aut autem porro et earum placeat ut error nobis eos labore incidunt est repellendus consequatur. Ex possimus corrupti ut maiores libero quo fugiat perferendis non maiores alias eum dolorem totam non possimus odit. Vel illo natus et nulla quaerat non Quis tempora ea sint consequuntur. </p><p>Sit assumenda pariatur eum nemo architecto vel perspiciatis amet et aliquid magni qui voluptate rerum ut facilis voluptates et provident velit. Qui molestiae nulla aut galisum possimus in nihil unde qui eligendi distinctio hic velit nihil. Eum quaerat eaque non neque ipsa est ipsa molestiae ut voluptatibus internos sit ipsa laudantium aut blanditiis quaerat in quis quaerat. Et quia iure hic culpa odio aut placeat galisum? </p><p>Id iusto dolores ut autem quis ex deleniti voluptates id nihil numquam non accusantium alias est explicabo sunt. A voluptas velit et modi officiis rem error excepturi et expedita obcaecati! Et nostrum consequatur ad autem ducimus quo harum cupiditate. Vel dolorum eius 33 rerum obcaecati nam totam esse ut incidunt sint? </p><p>Qui illo deleniti 33 dolores ratione et quia eius. Est natus sint nam veritatis possimus et temporibus repudiandae. Et inventore culpa qui nisi temporibus vel labore sint eos velit libero sit autem veniam aut illo dolor. </p><p>Aut nobis dolores et magni quod qui Quis recusandae. Qui repellendus quia ea doloribus repudiandae est magnam corrupti et voluptatem delectus ut iste explicabo. Est Quis fugiat et error asperiores cum veritatis nihil ea incidunt officiis rem provident possimus qui laborum libero qui odit impedit! Ut beatae nesciunt qui molestiae eveniet non iusto officia sit quis voluptatum aut architecto adipisci in maiores natus ut velit voluptatem. </p><p>Sed illum deserunt qui dolores dolores id officiis beatae At tenetur molestias eos corporis animi et provident blanditiis. Ut animi ipsa non galisum ipsam ut neque sint et soluta eligendi a quidem tempore. </p>
-
+        <div className='text-lg'>
+          software engineer
         </div>
       </div>
     </div>
