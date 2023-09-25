6c91dfa50b3d948c81ef326c17aa395c7a3766fe
thisisrick25 <swapnaneel06@gmail.com>
2023-09-25 15:08:12 +0530

    chore/refactor: abstract the styling

--- components/mdxContent.tsx
+++ components/mdxContent.tsx
@@ -1,31 +1,11 @@
 import { useMDXComponent } from 'next-contentlayer/hooks'
+import { H2, H3, H4, H5 } from '@/lib/mdxStyle';
 
 const components = {
-  h1: (props: any) => (
-    <h2
-      className="relative mt-8 pt-9 text-xl font-bold sm:text-3xl pb-2"
-      {...props}
-    />
-  ),
-  h2: (props: any) => (
-    <h3
-      className="relative mt-10 pt-9 text-xl font-extrabold sm:text-2xl pb-2"
-      {...props}
-    />
-  ),
-  h3: (props: any) => (
-    <h4
-      className="text-xl font-extrabold pb-1"
-      {...props}
-    />
-  ),
-  h4: (props: any) => (
-    <h5
-      className="text-lg font-bold pb-1"
-      {...props}
-    />
-  ),
-
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
 };
 
 // @ts-ignore
---
+++ lib/mdxStyle.tsx
@@ -0,0 +1,32 @@
+export function H2(props: any) {
+  return (
+    <h2
+      className="relative mt-8 pt-9 text-2xl font-bold pb-2"
+      {...props}
+    />
+  );
+}
+export function H3(props: any) {
+  return (
+    <h2
+      className="relative mt-10 pt-9 text-xl font-extrabold pb-2"
+      {...props}
+    />
+  );
+}
+export function H4(props: any) {
+  return (
+    <h2
+      className="text-xl font-extrabold pb-1"
+      {...props}
+    />
+  );
+}
+export function H5(props: any) {
+  return (
+    <h2
+      className="text-lg font-bold pb-1"
+      {...props}
+    />
+  );
+}
