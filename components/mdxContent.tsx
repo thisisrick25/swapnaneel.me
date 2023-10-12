c3c38e29e1b002f523271acd673a283438d576a5
thisisrick25 <swapnaneel06@gmail.com>
2023-10-12 19:39:21 +0530

    fix: delete useless mdx design

--- components/mdxContent.tsx
+++ components/mdxContent.tsx
@@ -1,11 +1,7 @@
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import { H2, H3, H4, H5 } from '@/lib/mdxStyle';
 
 const components = {
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  h5: H5,
+
 };
 
 // @ts-ignore
@@ -13,7 +9,7 @@ export default function MDXContent({ code }) {
   const MDX = useMDXComponent(code)
 
   return (
-    <div className="prose prose-quoteless prose-neutral dark:prose-invert">
+    <div className="prose prose-neutral max-w-max dark:prose-invert">
       <MDX components={components}/>
     </div>
   )
