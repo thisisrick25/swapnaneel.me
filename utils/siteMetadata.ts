98de60beaed826c973fd472627882f022e4cf2ef
thisisrick25 <swapnaneel06@gmail.com>
2023-10-22 21:52:53 +0530

    feat: add site metadeta

---
+++ utils/siteMetadata.ts
@@ -0,0 +1,20 @@
+// please update required information
+export const siteMetadata = {
+  title: 'Swapnaneel Patra',
+  author: 'Swapnaneel Patra',
+  headerTitle: 'Developer Extraordinaire',
+  description: 'Software developer and engineer. Creative thinker and a Technical expert',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://swapnaneel.me', // your website URL
+  siteLogo: '',
+  socialBanner: '', // add social banner in the public folder
+  email: '',
+  github: '',
+  twitter: '',
+  facebook: '',
+  youtube: '',
+  linkedin: '',
+  dribbble: '',
+  locale: 'en-US',
+}
