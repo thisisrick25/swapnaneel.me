9815e8207ce360c2946181339450a51c4ef12176
thisisrick25 <swapnaneel06@gmail.com>
2023-10-26 19:51:04 +0530

    fix: nocache

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,11 +31,9 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: true,
-      noarchive: true,
       noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
