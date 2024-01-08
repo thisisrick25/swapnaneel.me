e03ac54269b5ea79f07ac6216e44f6e31c87ccf9
thisisrick25 <swapnaneel06@gmail.com>
2024-01-08 21:51:09 +0530

    feat: add link functionality to tags in Tag component

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link"
+import githubSlugger from "github-slugger"
+
 //@ts-ignore
 export default function Tag({ blog }) {
+  const slugger = new githubSlugger()
+
   return (
     <div className="text-neutral-600 dark:text-neutral-400 text-base rounded-lg mb-4 overflow-hidden overflow-y-auto ">
       {//@ts-ignore
         blog.tags.map((tag, index) => {
           return (
             <>
-              <a href={`#${tag}`}>
+              <Link href={`/categories/${slugger.slug(tag)}`}>
                 <span className="hover:underline">{`#${tag}`}</span>
-              </a>
+              </Link>
               {index !== blog.tags.length - 1 && ", "} {/* Add a comma after each tag except the last one */}
             </>
           )
