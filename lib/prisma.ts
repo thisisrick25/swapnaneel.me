b49eff3545bbd995456f9abb79157a788ad9334d
thisisrick25 <swapnaneel06@gmail.com>
2024-02-08 22:31:15 +0530

    refactor: prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,31 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-// PrismaClient is attached to the `global` object in development
-// to prevent exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+const prismaClientSingleton = () => { return new PrismaClient() }
 
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
-// export default prisma;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== 'production')
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+// import { PrismaClient } from '@prisma/client'
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
