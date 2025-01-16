db9def68da3dc879ee7477d27a4c9e42d7c51fb4
thisisrick25 <swapnaneel06@gmail.com>
2025-01-16 21:57:52 +0530

    refactor: integrate MDX support into Next.js configuration
    - Added MDX support by incorporating the @next/mdx package into the Next.js configuration.
    - Updated the module export to include MDX processing alongside Content Collections.
    - This enhancement improves the project's capability to handle MDX files, facilitating richer content authoring.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const { withContentCollections } = require("@content-collections/next");
+// import createMDX from "@next/mdx";
+const createMDX = require("@next/mdx");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true  
 }
 
+const withMDX = createMDX({});
+
 // withContentCollections must be the outermost plugin
-module.exports = withContentCollections(nextConfig)
+module.exports = withContentCollections(withMDX(nextConfig))
