f4118597834cef6736bfd535cad6437d87bcfbdd
thisisrick25 <swapnaneel06@gmail.com>
2025-01-16 12:29:38 +0530

    refactor: update blog page to use Content Collections and improve metadata handling
    - Replaced import from 'contentlayer/generated' to 'content-collections'.
    - Enhanced type definitions for parameters in generateMetadata and BlogPage functions.
    - Extracted headings from blog content using a new utility function.
    - Updated TableOfContents component to accept headings instead of blog content.
    - Improved rendering of blog content and metadata display.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import { allBlogs } from 'contentlayer/generated'
+import { allBlogs } from 'content-collections'
 import { formatDate } from "@/lib/formatDate";
-import MDXContent from "@/components/mdxContent";
 import { notFound } from 'next/navigation';
 import TableOfContents from '@/components/tableOfContents';
 import Tag from '@/components/tag';
 import ViewCounter from '@/components/viewCounter';
+import { extractHeadings } from '@/lib/utils/extractHeadings';
 
 export async function generateStaticParams() {
   return allBlogs
@@ -14,9 +14,9 @@ export async function generateStaticParams() {
     }));
 }
 
-// @ts-ignore
-export async function generateMetadata({ params }) {
-  const blog = allBlogs.find((blog) => blog.slug === params.slug)
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { slug } = await params;
+  const blog = allBlogs.find((blog) => blog.slug === slug);
   if (!blog) {
     return;
   }
@@ -27,25 +27,28 @@ export async function generateMetadata({ params }) {
   };
 }
 
-// @ts-ignore
-export default function Page({ params }) {
-  const blog = allBlogs.find((blog) => blog.slug === params.slug);
-
+export default async function BlogPage({ params }: { params: { slug: string } }) {
+  const { slug } = await params;
+  const blog = allBlogs.find((blog) => blog.slug === slug);
   if (!blog) {
     notFound();
   }
 
+  const headings = extractHeadings(blog.content);
+
   return (
     <article>
       <div>
-        <p className='text-2xl font-bold'>{blog?.title}</p>
-        <div className='mb-4 grid grid-cols-2 text-lg text-neutral-600 dark:text-neutral-400'>
-          <p>{formatDate(blog?.publishedAt)}</p>
-          <ViewCounter slug={blog?.slug} />
+        <p className='text-2xl font-bold'>{blog.title}</p>
+        <div className='grid grid-cols-2 mb-4 text-lg text-neutral-600 dark:text-neutral-400'>
+          <p>{formatDate(blog.publishedAt)}</p>
+          <ViewCounter slug={blog.slug} />
         </div>
         <Tag blog={blog} />
-        <TableOfContents blog={blog} />
-        <MDXContent code={blog?.body.code}/>
+        <TableOfContents headings={headings} />
+        <div className="max-w-max prose prose-neutral dark:prose-invert">
+          {blog.content}
+        </div>
       </div>
     </article>
   )
