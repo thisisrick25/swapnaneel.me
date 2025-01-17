c604e52995ddf1852c7fce938eb3bb1baf6d7177
thisisrick25 <swapnaneel06@gmail.com>
2025-01-17 22:50:22 +0530

    refactor: update ThemeProvider props type for improved type safety
    - Changed the props type for the ThemeProvider component from ThemeProviderProps to React.ComponentProps of NextThemeProvider.
    - This update enhances type safety and ensures better compatibility with the NextThemeProvider's props, improving overall code quality.

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { ThemeProvider as NextThemeProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props}: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props}: React.ComponentProps<typeof NextThemeProvider>) {
   return (
     <NextThemeProvider attribute="class" defaultTheme="system" storageKey='theme' {...props}>
       {children}
