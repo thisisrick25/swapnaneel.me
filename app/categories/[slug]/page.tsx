e196c34b7c2d72623dda88323faccb9ba9a204de
thisisrick25 <swapnaneel06@gmail.com>
2025-01-17 21:06:27 +0530

    refactor: simplify static parameter generation and improve category slug handling
    - Removed async from the generateStaticParams function for better performance.
    - Updated the Page component to destructure and await params correctly, enhancing clarity.
    - Refactored the blog filtering logic to use the categorySlug directly, improving readability.
    - Cleaned up the rendering of blog links, ensuring consistent formatting and structure.

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { allBlogs } from "content-collections";
 import { slug } from "github-slugger"
 import Link from "next/link";
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   const allTags: string[] = [];
   allBlogs.forEach((blog) => {
     if (blog.isPublished) {
@@ -20,43 +20,38 @@ export async function generateStaticParams() {
   }));
 }
 
-//@ts-ignore
-export default function Page({ params }) {
-  const allTags: string[] = [];
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug: categorySlug } = await params;
   const blogs = allBlogs.filter((blog) => {
     return blog.tags?.some((tag) => {
       let slugified = slug(tag);
-      if (!allTags.includes(slugified)) {
-        allTags.push(slugified);
-      }
-      return slugified === params.slug;
+      return slugified === categorySlug;
     });
   });
 
   return (
     <>
       <div className='mb-8 space-y-3 text-2xl font-bold'>
-        {`#${params.slug}`}
+        {`#${categorySlug}`}
       </div>
       <div>
-        {blogs
-          .map((blog) => (
-            <Link
-              key={blog.slug}
-              className="grid grid-cols-1 mb-4"
-              href={`/blog/${blog.slug}`}
-            >
-              <div className="w-full">
-                <div>
-                  {blog.title}
-                </div>
-                <div className=' grid grid-cols-2 text-base text-neutral-600 dark:text-neutral-400'>
-                  <div>{formatDate(blog?.publishedAt)}</div>
-                  <div className=''>views</div>
-                </div>
+        {blogs.map((blog) => (
+          <Link
+            key={blog.slug}
+            className="grid grid-cols-1 mb-4"
+            href={`/blog/${blog.slug}`}
+          >
+            <div className="w-full">
+              <div>
+                {blog.title}
               </div>
-            </Link>
-          ))}
+              <div className='grid grid-cols-2 text-base text-neutral-600 dark:text-neutral-400'>
+                <div>{formatDate(blog.publishedAt)}</div>
+                <div className=''>views</div>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   );
