cc3bdfe6c4c789fe9cd225da05a2fe18327511b5
thisisrick25 <swapnaneel06@gmail.com>
2025-01-22 01:21:06 +0530

    refactor: clean up next.config.ts by removing unused remark and rehype plugins
    - Commented out unused remark and rehype plugin imports to streamline the configuration.
    - This change improves code clarity and organization, aligning with ongoing efforts to simplify the project's setup for MDX support and content collections.

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import { withContentCollections } from "@content-collections/next";
 import createMDX from "@next/mdx";
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-// import remarkGfm from "remark-gfm";
-// import rehypePrettyCode from 'rehype-pretty-code';
-// import rehypeSlug from 'rehype-slug';
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-// import { rehypePrettyCodeOptions, rehypeAutolinkHeadingsOptions } from './lib/rehypeOptions'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,10 +12,7 @@ const nextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkFrontmatter,remarkMdxFrontmatter],
-    // remarkPlugins: [remarkGfm],
-    // rehypePlugins: [[rehypeSlug],
-    //   [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions],
-    //   [rehypePrettyCode, rehypePrettyCodeOptions]],
+    rehypePlugins: [],
   },
 });
 
