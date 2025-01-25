4e4e205658be768b1ef01b43fb428dccbf8cce10
thisisrick25 <swapnaneel06@gmail.com>
2025-01-25 21:52:46 +0530

    fix: uncomment rehypePrettyCodeOptions methods for line and word highlighting

--- lib/rehypeOptions.ts
+++ lib/rehypeOptions.ts
@@ -7,20 +7,20 @@ export const rehypeAutolinkHeadingsOptions = {
 
 export const rehypePrettyCodeOptions = {
   theme: 'one-dark-pro',
-  // //@ts-ignore
-  // onVisitLine(node) {
-  //   // Prevent lines from collapsing in `display: grid` mode, and allow empty
-  //   // lines to be copy/pasted
-  //   if (node.children.length === 0) {
-  //     node.children = [{ type: 'text', value: ' ' }];
-  //   }
-  // },
-  // //@ts-ignore
-  // onVisitHighlightedLine(node) {
-  //   node.properties.className.push('line--highlighted');
-  // },
-  // //@ts-ignore
-  // onVisitHighlightedWord(node) {
-  //   node.properties.className = ['word--highlighted'];
-  // },
+  //@ts-ignore
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and allow empty
+    // lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+  },
+  //@ts-ignore
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('line--highlighted');
+  },
+  //@ts-ignore
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['word--highlighted'];
+  },
 }
