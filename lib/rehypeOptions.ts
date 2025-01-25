42889b996ad4367541f13ba1a9f5ca6694012051
thisisrick25 <swapnaneel06@gmail.com>
2025-01-25 22:04:39 +0530

    fix: remove TypeScript ignore comments from rehypePrettyCodeOptions methods

--- lib/rehypeOptions.ts
+++ lib/rehypeOptions.ts
@@ -7,20 +7,17 @@ export const rehypeAutolinkHeadingsOptions = {
 
 export const rehypePrettyCodeOptions = {
   theme: 'one-dark-pro',
-  //@ts-ignore
-  onVisitLine(node) {
+  onVisitLine(node: any) {
     // Prevent lines from collapsing in `display: grid` mode, and allow empty
     // lines to be copy/pasted
     if (node.children.length === 0) {
       node.children = [{ type: 'text', value: ' ' }];
     }
   },
-  //@ts-ignore
-  onVisitHighlightedLine(node) {
+  onVisitHighlightedLine(node: any) {
     node.properties.className.push('line--highlighted');
   },
-  //@ts-ignore
-  onVisitHighlightedWord(node) {
+  onVisitHighlightedWord(node: any) {
     node.properties.className = ['word--highlighted'];
   },
 }
