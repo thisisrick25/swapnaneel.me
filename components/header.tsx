1b0852eb740f8f1294e84ec03dac9f7df78e04d4
thisisrick25 <swapnaneel06@gmail.com>
2025-01-28 20:57:03 +0530

    refactor: remove unnecessary self-center class from header component

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Navbar from './navbar'
 export default function Header() {
   return (
     <header className='w-1/4 h-full fixed z-20 right-0 top-0 flex flex-col justify-center py-3'>
-      <div className='self-center'>
+      <div>
         <Navbar />
       </div>
     </header>
