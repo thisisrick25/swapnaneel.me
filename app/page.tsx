fa0f21591959e8430c937d6a0a6a665a37678f65
thisisrick25 <swapnaneel06@gmail.com>
2025-01-29 02:52:24 +0530

    refactor: update text content in Home component for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className='h-screen flex flex-col justify-center'>
       <div className='text-2xl font-bold'>
-        hello, im Swapnaneel
+        Swapnaneel
       </div>
       <div className=''>
-        software engineer
+        engineer, idealist and always learning
       </div>
     </div>
   )
