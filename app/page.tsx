a380f672709559ef2b5c2cbbec3a6f9e98a742ce
thisisrick25 <swapnaneel06@gmail.com>
2025-01-28 21:23:53 +0530

    refactor: update layout of Home component for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='grid grid-cols-1'>
+    <div className='h-screen flex flex-col justify-center'>
       <div className='text-2xl font-bold'>
         hello, im Swapnaneel
       </div>
