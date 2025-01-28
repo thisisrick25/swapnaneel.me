dc1a6873ad3ff55598458fa7996feea1d9846770
thisisrick25 <swapnaneel06@gmail.com>
2025-01-28 20:56:36 +0530

    refactor: update header layout for improved positioning and responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Navbar from './navbar'
 
 export default function Header() {
   return (
-    <header className='w-full sticky z-20 top-0 grid grid-cols-2 justify-between py-3 bg-white/30 dark:bg-black/30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg mb-12'>
+    <header className='w-1/4 h-full fixed z-20 right-0 top-0 flex flex-col justify-center py-3'>
       <div className='self-center'>
         <Navbar />
       </div>
