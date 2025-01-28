198e43a6d065bd48db4acf99de4562fb1ac7ec3f
thisisrick25 <swapnaneel06@gmail.com>
2025-01-28 20:42:25 +0530

    refactor: remove homepage logo, i dont like it 💩

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link'
 import Navbar from './navbar'
-import { HomeLogo } from './homeLogo'
 
 export default function Header() {
   return (
     <header className='w-full sticky z-20 top-0 grid grid-cols-2 justify-between py-3 bg-white/30 dark:bg-black/30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg mb-12'>
-      <Link
-        href='/' aria-label='Homepage'>
-        <HomeLogo />
-      </Link>
       <div className='self-center'>
         <Navbar />
       </div>
