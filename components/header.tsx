9d50e1625a15c095f4821894ee0e960ff1130ebe
thisisrick25 <swapnaneel06@gmail.com>
2025-01-30 02:47:42 +0530

    refactor: replace Link import from next/link with next-view-transitions for improved navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link"
+// import Link from "next/link"
+import { Link } from "next-view-transitions"
 import { HEADER_LINKS } from '@/config/links'
 import ThemeSwitch from "./themeSwitch"
 
