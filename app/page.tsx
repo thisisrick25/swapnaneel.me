3c8f7f1a3377856b405c8f9a9901ff37559380ab
thisisrick25 <swapnaneel06@gmail.com>
2025-01-30 00:39:08 +0530

    refactor: update layout and styling in Home component for improved readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className='h-screen flex flex-col justify-center'>
-      <div className='text-2xl font-bold'>
+    <div>
+      <div className='text-xl font-bold mb-4'>
         Swapnaneel
       </div>
       <div className=''>
