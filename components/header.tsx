0c9b0775500861b369d717941714c6304f69fd76
thisisrick25 <swapnaneel06@gmail.com>
2025-02-01 23:59:53 +0530

    refactor: update import path for HEADER_LINKS in Header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 // import Link from "next/link"
 import { Link } from "next-view-transitions"
-import { HEADER_LINKS } from '@/config/links'
+import { HEADER_LINKS } from 'config/links'
 import ThemeSwitch from "./themeSwitch"
 
 export default function Header() {
