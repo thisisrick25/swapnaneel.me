6a2cd2d9e0b7e8bb6d342c0d3b445b8713e1480a
thisisrick25 <swapnaneel06@gmail.com>
2025-02-26 13:00:20 +0530

    refactor: enhance MDX configuration with additional remark and rehype plugins

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import createMDX from "@next/mdx";
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import remarkGfm from 'remark-gfm'
+import rehypeSlug from 'rehype-slug'
+import rehypePrettyCode from "rehype-pretty-code";
+import { rehypeAutolinkHeadingsOptions, rehypePrettyCodeOptions } from '@/lib/rehypeOptions'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,8 +15,8 @@ const nextConfig = {
 
 const withMDX = createMDX({
   options: {
-    // remarkPlugins: [remarkFrontmatter,remarkMdxFrontmatter],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions]],
   },
 });
 
