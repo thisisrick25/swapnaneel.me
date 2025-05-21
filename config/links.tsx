a515d3b01e67ba93a6b928c235cde34b192d5e62
thisisrick25 <swapnaneel06@gmail.com>
2025-05-22 02:05:05 +0530

    refactor: comment out unused links in HEADER_LINKS for clarity

--- config/links.tsx
+++ config/links.tsx
@@ -6,14 +6,14 @@ type HeaderLink = {
 
 export const HEADER_LINKS: HeaderLink[] = [
   { href: '/posts', text: 'Posts' },
-  { href: '/projects', text: 'Projects' },
-  { href: '/more', text: 'Stuffs',
-    children: [
-      { href: '/more/commonplace-book', text: 'Commonplace Book' },
-      { href: '/more/reading-list', text: 'Reading List' },
-      { href: '/more/ideas', text: 'Ideas' },
-      { href: '/more/bucket-list', text: 'Bucket List' },
-      { href: '/more/courses', text: 'Courses' },
-    ]
-  }
+  // { href: '/projects', text: 'Projects' },
+  // { href: '/more', text: 'Stuffs',
+  //   children: [
+  //     { href: '/more/commonplace-book', text: 'Commonplace Book' },
+  //     { href: '/more/reading-list', text: 'Reading List' },
+  //     { href: '/more/ideas', text: 'Ideas' },
+  //     { href: '/more/bucket-list', text: 'Bucket List' },
+  //     { href: '/more/courses', text: 'Courses' },
+  //   ]
+  // }
 ]
