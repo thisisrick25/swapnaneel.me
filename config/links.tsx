c9106c06c9149254cc60abf80b507da393f373da
thisisrick25 <swapnaneel06@gmail.com>
2025-05-21 23:14:54 +0530

    refactor: update HeaderLink type definition and restructure HEADER_LINKS for better clarity

--- config/links.tsx
+++ config/links.tsx
@@ -1,15 +1,19 @@
-type HeaderLinks = {
+type HeaderLink = {
   href: string
   text: string
-}[]
+  children?: HeaderLink[]
+}
 
-export const HEADER_LINKS: HeaderLinks = [
+export const HEADER_LINKS: HeaderLink[] = [
   { href: '/blog', text: 'Posts' },
   { href: '/projects', text: 'Projects' },
-  { href: '/more', text: 'Stuffs'},
-  // { href: '/commonplace-book', text: 'Commonplace Book' },
-  // { href: '/reading-list', text: 'Reading List' },
-  // { href: '/ideas', text: 'Ideas' },
-  // { href: '/bucket-list', text: 'Bucket List' },
-  // { href: '/courses', text: 'Courses' },
+  { href: '/more', text: 'Stuffs',
+    children: [
+      { href: '/commonplace-book', text: 'Commonplace Book' },
+      { href: '/reading-list', text: 'Reading List' },
+      { href: '/ideas', text: 'Ideas' },
+      { href: '/bucket-list', text: 'Bucket List' },
+      { href: '/courses', text: 'Courses' },
+    ]
+  }
 ]
