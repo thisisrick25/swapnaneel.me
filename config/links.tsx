8b21468d035d4cd26f8ed6385d5bb654954c2a86
thisisrick25 <swapnaneel06@gmail.com>
2025-05-21 21:43:24 +0530

    refactor: update link text for blog and adjust header links structure

--- config/links.tsx
+++ config/links.tsx
@@ -4,11 +4,12 @@ type HeaderLinks = {
 }[]
 
 export const HEADER_LINKS: HeaderLinks = [
-  { href: '/blog', text: 'Blog' },
+  { href: '/blog', text: 'Posts' },
   { href: '/projects', text: 'Projects' },
-  { href: '/commonplace-book', text: 'Commonplace Book' },
-  { href: '/reading-list', text: 'Reading List' },
-  { href: '/ideas', text: 'Ideas' },
-  { href: '/bucket-list', text: 'Bucket List' },
-  { href: '/courses', text: 'Courses' },
+  { href: '/more', text: 'Stuffs'},
+  // { href: '/commonplace-book', text: 'Commonplace Book' },
+  // { href: '/reading-list', text: 'Reading List' },
+  // { href: '/ideas', text: 'Ideas' },
+  // { href: '/bucket-list', text: 'Bucket List' },
+  // { href: '/courses', text: 'Courses' },
 ]
