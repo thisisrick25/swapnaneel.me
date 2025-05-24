06e2092915713d2c1aa466f8422c15b06b805509
thisisrick25 <swapnaneel06@gmail.com>
2025-05-24 22:49:06 +0530

    feat: add Footer component with social media links

---
+++ components/footer.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { siteMetadata } from '@/utils/siteMetadata'
+
+export default function Footer() {
+  return (
+    <footer className="mx-auto mt-16 flex flex-col items-center">
+      <div className="mb-3 flex space-x-4">
+        <Link
+          href={siteMetadata.linkedin}
+          target="_blank"
+          className="text-sm text-neutral-600 transition hover:text-neutral-400"
+        >
+          linkedin
+        </Link>
+        <Link
+          href={siteMetadata.github}
+          target="_blank"
+          className="text-sm text-neutral-600 transition hover:text-neutral-400"
+        >
+          github
+        </Link>
+      </div>
+    </footer>
+  )
+}
