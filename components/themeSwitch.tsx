f5258f1f6d84c00ecf7d4dd539a37828aa4e6bff
thisisrick25 <swapnaneel06@gmail.com>
2025-05-27 00:54:22 +0530

    fix: update button hover styles

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -22,7 +22,7 @@ export default function ThemeSwitch() {
       title="Day 'n' Nite"
       aria-label="Toggle dark mode"
       onClick={handleThemeChange}
-      className="px-2 py-2 text-black dark:text-white inline-flex items-center justify-center h-9 w-9 rounded-md hover:bg-neutral-400 dark:hover:bg-neutral-600 duration-300 ease-in transition-colors"
+      className="px-2 py-2 text-black dark:text-white inline-flex items-center justify-center h-9 w-9 rounded-md hover:bg-neutral-400/80 dark:hover:bg-neutral-600/80 duration-300 ease-in transition-colors"
     >
       <span className="sr-only">Toggle theme</span>
       <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
