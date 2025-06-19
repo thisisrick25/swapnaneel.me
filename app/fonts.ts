418a2764bdd8c49ce9ca28e1c0a0211dc780e7a7
thisisrick25 <swapnaneel06@gmail.com>
2025-06-20 03:39:42 +0530

    perf: Optimize font loading and variable usage
    - Configure fonts to use CSS variables by adding the variable property.
    - Specify style variants (normal/italic) for fonts to ensure correct loading.
    - Reduce Inter font weight to 400 to optimize bundle size.
    - These updates enhance font loading performance and provide more flexible styling options.

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,39 +3,50 @@ import { Roboto_Mono, Raleway, Montserrat, Inter, IBM_Plex_Mono, Poppins, Space_
 const roboto_mono = Roboto_Mono({
   weight: '400',
   subsets: ['latin'],
+  style: ['italic', 'normal'],
   variable: '--font-roboto-mono',
 })
 
 const raleway = Raleway({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
+  style: ['italic', 'normal'],
+  variable: '--font-raleway',
 })
 
 const montserrat = Montserrat({
   weight: '500',
   subsets: ['latin'],
+  style: ['italic', 'normal'],
   variable: '--font-montserrat',
 })
 
 const inter = Inter({
-  weight: ['400', '500', '600', '700'],
+  weight: '400',
   subsets: ['latin'],
+  style: ['italic', 'normal'],
+  variable: '--font-inter',
 })
 
 const ibm_plex_mono = IBM_Plex_Mono({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
+  style: ['italic', 'normal'],
+  variable: '--font-ibm-plex-mono',
 })
 
 const poppins = Poppins({
   weight: ['300'],
-  style: ['italic', 'normal'],
   subsets: ['latin'],
+  style: ['italic', 'normal'],
+  variable: '--font-poppins',
 })
 
 const space_grotesk = Space_Grotesk({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
+  style: ['normal'],
+  variable: '--font-space-grotesk',
 })
 
 export {
