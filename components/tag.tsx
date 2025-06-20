df9bc0a97746d859a80baa16ca3a9993a503a894
thisisrick25 <swapnaneel06@gmail.com>
2025-06-21 01:08:51 +0530

    style: move hover underline to individual tag links
    - Previously, the hover:underline class was applied to the parent div element.
    - Ensures that only the specific tag being hovered over will display the underline effect, improving user experience and visual precision.

--- components/tag.tsx
+++ components/tag.tsx
@@ -10,11 +10,12 @@ export default function Tag({ blog }: { blog: BlogType }) {
   const slugger = new githubSlugger()
 
   return (
-    <div className={`${poppins.className} text-neutral-600 dark:text-neutral-400 text-sm rounded-lg mb-4 overflow-hidden overflow-y-auto hover:underline`} style={{ fontWeight: '300' }}>
+    <div className={`${poppins.className} text-neutral-600 dark:text-neutral-400 text-sm rounded-lg mb-4 overflow-hidden overflow-y-auto`} style={{ fontWeight: '300' }}>
       {blog.tags.map((tag, index) => (
         <Link 
           key={`${tag}-${index}`}
           href={`/categories/${slugger.slug(tag)}`}
+          className="hover:underline"
         >
           {`#${tag}`}
           {index !== blog.tags.length - 1 && ", "}
