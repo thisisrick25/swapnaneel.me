207cf887fbc4bf0c64f58a74a9a80cea23216dd5
thisisrick25 <swapnaneel06@gmail.com>
2025-10-23 03:22:15 +0530

    fix(github): set default branch to 'main', fetch raw file contents and shorten revalidation to 1800

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 // GitHub Repository Details
 export const GITHUB_REPO_OWNER = 'thisisrick25'
 export const GITHUB_REPO_NAME = 'posts'
-export const GITHUB_BRANCH = '' // Default branch, usually 'main' or 'master', leave blank for default branch
+export const GITHUB_BRANCH = 'main' // Default branch, usually 'main' or 'master', leave blank for default branch
 export const GITHUB_CONTENT_PATH = '' // Path to the content directory, e.g., 'content/posts', leave blank for root directory
 
 // GitHub API & Webhook
