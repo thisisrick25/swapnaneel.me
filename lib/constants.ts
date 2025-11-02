70a6650aa42f8c950c06543533cbd0f98dc5dc66
thisisrick25 <swapnaneel06@gmail.com>
2025-11-02 22:18:54 +0530

    chore(constants): update GITHUB_REPO_NAME to 'brain'

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 // GitHub Repository Details
 export const GITHUB_REPO_OWNER = 'thisisrick25'
-export const GITHUB_REPO_NAME = 'posts'
+export const GITHUB_REPO_NAME = 'brain'
 export const GITHUB_BRANCH = 'main' // Default branch, usually 'main' or 'master', leave blank for default branch
 export const GITHUB_CONTENT_PATH = '' // Path to the content directory, e.g., 'content/posts', leave blank for root directory
 
