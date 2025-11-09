f5af99b161e862f3c61592aa788faa0801498757
thisisrick25 <swapnaneel06@gmail.com>
2025-11-09 23:59:59 +0530

    refactor(loading): adjust loading item layout for improved visual consistency

--- app/posts/loading.tsx
+++ app/posts/loading.tsx
@@ -5,11 +5,9 @@ export default function Loading() {
         <a key={i} className="grid grid-cols-1 w-full my-4">
           <div className="bg-neutral-100/60 dark:bg-neutral-900/60 p-4 rounded-lg shadow-xs">
             <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded mb-2"></div>
-            <div className="text-xs md:text-sm">
-              <div className="flex justify-between">
-              <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/4"></div>
-              <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/6"></div>
-              </div>
+            <div className="flex justify-between">
+              <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-1/4"></div>
+              <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-1/6"></div>
             </div>
           </div>
         </a>
