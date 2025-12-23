ed123968e50640e606b2bd574c340e1f06d3f240
thisisrick25 <swapnaneel06@gmail.com>
2025-12-23 23:10:08 +0530

    refactor: simplify Footer component by removing links and adjusting layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,9 @@
-import Link from 'next/link'
-import { siteMetadata } from '@/utils/siteMetadata'
-
 export default function Footer() {
   return (
-    <footer className="mx-auto mt-16 flex flex-col items-center">
-      <div className="mb-3 flex space-x-4">
-        <Link
-          href={siteMetadata.linkedin}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xs md:text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 transition-colors duration-300"
-        >
-          linkedin
-        </Link>
-        <Link
-          href={siteMetadata.github}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xs md:text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 transition-colors duration-300"
-        >
-          github
-        </Link>
-      </div>
+    <footer className="py-16 pb-24 text-center">
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        © {new Date().getFullYear()} Swapnaneel Patra
+      </p>
     </footer>
   )
 }
