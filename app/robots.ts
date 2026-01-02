cfd020c0c2241d390c153f9a21c36ec4a01fefa3
thisisrick25 <swapnaneel06@gmail.com>
2026-01-02 17:46:12 +0530

    feat: add robots.txt configuration for SEO optimization

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+import { siteMetadata } from '@/utils/siteMetadata'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+  }
+}
