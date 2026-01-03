48834eb6980c653d70dc24ea38939a0ac3c0ac43
thisisrick25 <swapnaneel06@gmail.com>
2026-01-03 21:51:07 +0530

    feat: enhance theme transitions for smoother user experience

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -2,9 +2,15 @@
 
 import { ThemeProvider as NextThemeProvider } from "next-themes"
 
-export function ThemeProvider({ children, ...props}: React.ComponentProps<typeof NextThemeProvider>) {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemeProvider>) {
   return (
-    <NextThemeProvider attribute="class" defaultTheme="system" storageKey='theme' {...props}>
+    <NextThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      storageKey='theme'
+      disableTransitionOnChange={false}
+      {...props}
+    >
       {children}
     </NextThemeProvider>
   )
